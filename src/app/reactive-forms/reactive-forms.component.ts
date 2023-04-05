10d4c05833b947bc8f15884dfc51d33b9020a6e8
filipecajado <filipecajado@ucl.br>
2023-04-05 15:37:03 -0300

    Validators ReactiveForms

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
   styleUrls: ['./reactive-forms.component.scss']
 })
-export class ReactiveFormsComponent {
+export class ReactiveFormsComponent implements OnInit {
 
+  public cadastroForm: FormGroup = this.formbuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, Validators.email]]
+  })
+
+  constructor(private formbuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  public submitForm() {
+    if (this.cadastroForm.valid) {
+      console.log(this.cadastroForm.value);
+      console.log(this.cadastroForm.value.firstName);
+      console.log(this.cadastroForm.value.lastName);
+    }
+  }
 }
